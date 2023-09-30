28d04a29962aced3808694f46c16729e6a1c805d
jpstudy1988 <69568139+jpstudy1988@users.noreply.github.com>
2023-09-30 20:01:47 +0530

    added security

---
+++ src/main/java/com/in28minutes/ranga/socialmediaapplication/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.in28minutes.ranga.socialmediaapplication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails rootUser = User.builder()
+                .username("Jpb")
+                .password(passwordEncoder().encode("root"))
+                .roles("ADMIN")
+                .build();
+        UserDetails dev = User.builder()
+                .username("Barsha")
+                .password(passwordEncoder().encode("dev123"))
+                .roles("DEV")
+                .build();
+        return new InMemoryUserDetailsManager(rootUser,dev);
+    }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
